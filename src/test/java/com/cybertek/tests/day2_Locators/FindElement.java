146f39716882af4cc1da31182e3ba8102bd7b800
Olha Lytovka <olha@Olhas-MacBook-Air.local>
2020-03-03 20:28:44 -0500

    FindElement.java

--- src/test/java/com/cybertek/tests/day2_Locators/FindElement.java
+++ src/test/java/com/cybertek/tests/day2_Locators/FindElement.java
@@ -1,7 +1,32 @@
 package com.cybertek.tests.day2_Locators;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FindElement {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://cybertekschool.okta.com/");
+
+        driver.manage().window().maximize();
+
+        Thread.sleep(3000);
+
+
+        WebElement emailbox = driver.findElement(By.id("okta-signin-username"));
+        emailbox.sendKeys("olhalytovka@gmail.com");
+
+        //
+        driver.findElement(By.id("okta-signin-password")).sendKeys("password");
+
+
+        WebElement push = driver.findElement(By.id("okta-signin-submit"));
+        push.click();
 
     }
 }
